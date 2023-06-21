83fc342a879988d30173d8ccbc0410cc8d8aea8d
fkolar <fkolar@users.noreply.github.com>
2023-06-21 15:40:29 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# dynamic-page-focus
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-t6seax-x5sl7q)
---
+++ src/app/dynamic-page-tabs-example.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+import { MessageToastService } from '@fundamental-ngx/core/message-toast';
+
+@Component({
+    selector: 'fd-dynamic-page-tabs-example',
+    templateUrl: './dynamic-page-tabs-example.component.html',
+    styles: [
+        `
+          
+        `
+    ]
+})
+export class DynamicPageTabsExampleComponent {
+    visible = false;
+
+    stackedTabs = false;
+
+    pageTitle = 'Balenciaga Tripple S Trainers';
+
+    constructor(private _messageToastService: MessageToastService) {}
+
+    onCollapseChange(): void {
+        console.log('collapse changed');
+    }
+
+    openPage(stacked: boolean): void {
+        this.stackedTabs = stacked;
+        this.visible = true;
+        this._openToast('Dynamic Page has been opened');
+    }
+
+    closePage(): void {
+        this.visible = false;
+    }
+
+    handleAction(action: string): void {
+        this.closePage();
+        this._openToast(action);
+    }
+
+    private _openToast(content: string): void {
+        this._messageToastService.open(content, { duration: 3000 });
+    }
+}
